4072d46d36db5948c1bdbef8124483c6759a173d
Mina Takle Stensaker <mina@dhcp-10-22-75-138.wlan.ntnu.no>
2018-11-12 12:47:23 +0100

    updated motobs functionalities

--- motob.py
+++ motob.py
@@ -4,27 +4,32 @@ from motors import *
 class Motob():
 
     def __init__(self):
-        self.motors = Motors()
-        self.value = []
+        self.motors = Motors()                      #List of the motors, settings will be determined by this class
+        self.value = []                             #Holder of most recent motor recommendations
 
-
+    #Receives a new recommendation, loads to the value slot and operationalizes it
+    #new_rec = [function, speed] where speed is in range [-1,1]
     def update(self, new_rec):
         self.value = [new_rec]
         self.operationalize(new_rec)
 
-
+    #Converts a motor recommendation into motor settings, which are sent to the corresponing motors
     def operationalize(self, new_rec):
         if new_rec in self.value:
             if new_rec[0] == "F":
                 self.motors.forward(new_rec[1], 1)
             elif new_rec[0] == "B":
                 self.motors.backward(new_rec[1], 1)
-            elif new_rec[0] == "L":
-                self.motors.left(new_rec[1], 1)
-            elif new_rec[0] == "R":
-                self.motors.right(new_rec[1], 1)
             elif new_rec[0] == "S":
                 self.motors.stop()
+            elif new_rec[0] == "L":
+                self.motors.set_value([0, new_rec[1]], 1)
+            elif new_rec[0] == "R":
+                self.motors.set_value([new_rec[1], 0], 1)
+            elif new_rec[0] == "TL":
+                self.motors.left(new_rec[1], 1)
+            elif new_rec[0] == "TR":
+                self.motors.right(new_rec[1], 1)
 
 
 
